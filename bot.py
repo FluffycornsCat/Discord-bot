579d8cd1ac8fd3b9e898cf8a987913a3c5ea8bf2
MatthewXcv <39726487+MatthewXcv@users.noreply.github.com>
2020-01-19 21:09:08 +0100

    ok
    ok

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 
-# bot.py
 import os
+import random
 
 import discord
 from dotenv import load_dotenv
@@ -13,16 +13,44 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-
+    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
     print(
         f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
+        f'{guild.name}(id: {guild.id})\n'
     )
 
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
+
+    print(f'\n{client.user.name} has connected to Discord!')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+
+    if message.content == 'dat':
+        response = 'ass :^)'
+        await message.channel.send(response)
 
 client.run(TOKEN)
